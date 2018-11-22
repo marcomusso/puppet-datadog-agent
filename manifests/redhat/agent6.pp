ef65754e132624f63746d06400b5a6980b0275c3
Marco Musso <marco.musso@tomtom.com>
2018-11-22 17:46:58 +0100

    Removed remote_file references and module

--- manifests/redhat/agent6.pp
+++ manifests/redhat/agent6.pp
@@ -12,24 +12,8 @@ class datadog_agent::redhat::agent6(
 
   validate_bool($manage_repo)
   if $manage_repo {
-    $public_key_local = '/etc/pki/rpm-gpg/DATADOG_RPM_KEY.public'
 
     validate_string($baseurl)
-
-    remote_file { 'DATADOG_RPM_KEY.public':
-        owner  => root,
-        group  => root,
-        mode   => '600',
-        path   => $public_key_local,
-        source => $gpgkey
-    }
-
-    exec { 'install-gpg-key':
-        command => "/bin/rpm --import ${public_key_local}",
-        onlyif  => "/usr/bin/gpg --quiet --with-fingerprint -n ${public_key_local} | grep \'A4C0 B90D 7443 CF6E 4E8A  A341 F106 8E14 E094 22B3\'",
-        unless  => '/bin/rpm -q gpg-pubkey-e09422b3',
-        require => Remote_file['DATADOG_RPM_KEY.public'],
-    }
 
     yumrepo {'datadog':
       enabled  => 1,
@@ -37,7 +21,6 @@ class datadog_agent::redhat::agent6(
       gpgkey   => 'https://yum.datadoghq.com/DATADOG_RPM_KEY.public',
       descr    => 'Datadog, Inc.',
       baseurl  => $baseurl,
-      require  => Exec['install-gpg-key'],
     }
 
     Package { require => Yumrepo['datadog']}
